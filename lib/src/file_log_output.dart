58f7a24e24d81fcff425027f355980c261252e58
Hamid Marzbani <h.marzbani@bettechnology.com>
2024-05-23 14:20:47 +0330

    document: comments added to the project.

--- lib/src/file_log_output.dart
+++ lib/src/file_log_output.dart
@@ -1,22 +1,41 @@
 import 'dart:io';
 import 'logger_interface.dart';
 
+/// Log output implementation that writes log messages to a file.
 class FileLogOutput implements LogOutput {
+  /// Creates an instance of FileLogOutput with the specified file path.
+  ///
+  /// @param filePath The path to the log file where messages will be written.
   FileLogOutput(String filePath) : _logFile = File(filePath);
+
+  // The log file where messages will be written.
   final File _logFile;
 
+  /// Outputs a log message with the specified log level to the log file.
+  ///
+  /// @param level The log level of the message.
+  /// @param message The message to be logged.
+  /// @param exception An optional exception associated with the message.
+  /// @param stackTrace An optional stack trace associated with the message.
   @override
   void output(LogLevel level, String message,
       {Exception? exception, StackTrace? stackTrace}) {
     final logMessage = '${DateTime.now()} [$level]: $message';
     final buffer = StringBuffer(logMessage);
+
+    // Append exception details if provided.
     if (exception != null) {
       buffer.writeln('\nException: $exception');
     }
+
+    // Append stack trace if provided.
     if (stackTrace != null) {
       buffer.writeln('\nStackTrace: $stackTrace');
     }
+
     buffer.writeln();
+
+    // Write the log message to the file.
     _logFile.writeAsStringSync(buffer.toString(), mode: FileMode.append);
   }
 }
--- lib/src/logger_impl.dart
+++ lib/src/logger_impl.dart
@@ -1,18 +1,31 @@
 import 'console_log_output.dart';
 import 'logger_interface.dart';
 
+/// Logger implementation that logs messages based on a specified log level.
+/// Uses a LogOutput to determine where the log messages are outputted.
 class Logger implements ILogger {
+  // Default log output set to ConsoleLogOutput.
   LogOutput _output = ConsoleLogOutput();
+  // Default log level set to debug.
   LogLevel _logLevel = LogLevel.debug;
 
+  /// Logs a debug message if the current log level allows it.
+  ///
+  /// @param message The debug message to be logged.
+  /// @param exception An optional exception associated with the debug message.
+  /// @param stackTrace An optional stack trace associated with the debug message.
   @override
   void logDebug(String message, {Exception? exception, StackTrace? stackTrace}) {
     if (_logLevel.index <= LogLevel.debug.index) {
-
       _output.output(LogLevel.debug, message, exception: exception, stackTrace: stackTrace);
     }
   }
 
+  /// Logs an informational message if the current log level allows it.
+  ///
+  /// @param message The informational message to be logged.
+  /// @param exception An optional exception associated with the informational message.
+  /// @param stackTrace An optional stack trace associated with the informational message.
   @override
   void logInfo(String message, {Exception? exception, StackTrace? stackTrace}) {
     if (_logLevel.index <= LogLevel.info.index) {
@@ -20,6 +33,11 @@ class Logger implements ILogger {
     }
   }
 
+  /// Logs a warning message if the current log level allows it.
+  ///
+  /// @param message The warning message to be logged.
+  /// @param exception An optional exception associated with the warning message.
+  /// @param stackTrace An optional stack trace associated with the warning message.
   @override
   void logWarning(String message, {Exception? exception, StackTrace? stackTrace}) {
     if (_logLevel.index <= LogLevel.warning.index) {
@@ -27,6 +45,11 @@ class Logger implements ILogger {
     }
   }
 
+  /// Logs an error message if the current log level allows it.
+  ///
+  /// @param message The error message to be logged.
+  /// @param exception An optional exception associated with the error message.
+  /// @param stackTrace An optional stack trace associated with the error message.
   @override
   void logError(String message, {Exception? exception, StackTrace? stackTrace}) {
     if (_logLevel.index <= LogLevel.error.index) {
@@ -34,6 +57,11 @@ class Logger implements ILogger {
     }
   }
 
+  /// Logs a fatal error message if the current log level allows it.
+  ///
+  /// @param message The fatal error message to be logged.
+  /// @param exception An optional exception associated with the fatal error message.
+  /// @param stackTrace An optional stack trace associated with the fatal error message.
   @override
   void logFatal(String message, {Exception? exception, StackTrace? stackTrace}) {
     if (_logLevel.index <= LogLevel.fatal.index) {
@@ -41,11 +69,17 @@ class Logger implements ILogger {
     }
   }
 
+  /// Sets the output destination for log messages.
+  ///
+  /// @param output The output destination to be set.
   @override
   void setOutput(LogOutput output) {
     _output = output;
   }
 
+  /// Sets the log level for filtering log messages.
+  ///
+  /// @param level The log level to be set.
   @override
   void setLogLevel(LogLevel level) {
     _logLevel = level;
